5bb15c7f88af682dfe8b5911daff38ac1f278c03
Masrafe-Siam <masrafesiammbhs1633@gmail.com>
2024-10-03 18:51:49 +0600

    pdeosphere server issue solved

--- PEDOSPHERE/pedosphere_server/server/util.py
+++ PEDOSPHERE/pedosphere_server/server/util.py
@@ -5,7 +5,6 @@ import numpy as np
 __model = None
 __data_columns = None
 
-
 def get_predicted_plant_type(soil_temp, soil_ph):
     if soil_temp < 0 or soil_temp > 50:
         return 'Invalid soil temperature value'
@@ -16,7 +15,6 @@ def get_predicted_plant_type(soil_temp, soil_ph):
     x = np.array([soil_temp, soil_ph]).reshape(1, -1)
     return __model.predict(x)[0]  # Fixed to predict properly
 
-
 def load_saved_artifacts():
     print("Loading saved artifacts... start")
     global __data_columns
@@ -25,14 +23,12 @@ def load_saved_artifacts():
         __data_columns = json.load(f)['data_columns']
     global __model
     if __model is None:
-        with open('./artifacts/atmosphere_model.pickle', 'rb') as f:
+        with open('./artifacts/pedosphere_model.pickle', 'rb') as f:
             __model = pickle.load(f)
     print("Loading saved artifacts... done")
 
-
 def get_data_columns():
     return __data_columns
-
 
 if __name__ == '__main__':
     load_saved_artifacts()
